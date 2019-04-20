bba42ad1ddb8bb555923d643616c576e5cb1a208
Junior Farias <junior13farias@gmail.com>
2019-04-20 18:46:12 -0300

    Criação do arquivo V02 para inserir as categorias

---
+++ src/main/resources/db/migration/V02__insere_categorias.sql
@@ -0,0 +1,5 @@
+INSERT INTO categoria (nome) VALUES ('Lazer');
+INSERT INTO categoria (nome) VALUES ('Alimentação');
+INSERT INTO categoria (nome) VALUES ('Supermercado');
+INSERT INTO categoria (nome) VALUES ('Farmácia');
+INSERT INTO categoria (nome) VALUES ('Outros');
