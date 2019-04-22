be5036ef1e4d4bffc1bde52575650dc6580d4709
Junior Farias <junior13farias@gmail.com>
2019-04-22 01:26:55 -0300

    Criação dos recurso Pessoa, cadastro e listagem pelo Postman

---
+++ src/main/resources/db/migration/V04__insere_pessoas.sql
@@ -0,0 +1,20 @@
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('João da Silva','Av. Coronel Miguel Dias','50','Perto do Shopping da Pizza','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Cristiano Sobrinho','Av. Coronel Miguel Dias','100','Perto do Shopping da Pizza','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Felipe Soares','Av. Coronel Miguel Dias','200','Perto do Azilados','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Felipe Alexandre','Av. Coronel Miguel Dias','400','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Felipe Belchior','Av. Coronel Miguel Dias','401','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Carlos Galliza','Av. Coronel Miguel Dias','402','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Saulo Crazy Eyes','Av. Coronel Miguel Dias','403','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Gene Flides','Av. Coronel Miguel Dias','404','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Digão Cachorrão','Av. Coronel Miguel Dias','405','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
+INSERT INTO pessoa(nome,logradouro,numero,complemento,bairro,cep,cidade,estado,ativo) 
+VALUES('Gibrownie','Av. Coronel Miguel Dias','406','Perto do Shopping Iguatemi','Guararapes','65552-888','Fortaleza','Ceará',1);
