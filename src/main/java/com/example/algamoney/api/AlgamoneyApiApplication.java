1eaaf3f6aa3751539574093048092191a9da4f04
Junior Farias <junior13farias@gmail.com>
2019-04-24 13:17:48 -0300

    Deletando usuários

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -2,12 +2,24 @@ package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
+		
 	}
+	@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/WEB-INF/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(1);
+        return messageSource;
+    }
 
 }
